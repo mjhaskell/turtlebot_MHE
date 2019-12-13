09090f93d021a3481adc174e5e467230fbdaf57b
Mat Haskell <mhaskell9@gmail.com>
2019-12-12 20:28:54 -0700

    Added basic plotting script

--- python/plot_results.py
+++ python/plot_results.py
@@ -1,5 +1,11 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 lms = np.loadtxt('/tmp/MHE_landmarks.txt')
 poses = np.loadtxt('/tmp/MHE_outputs.txt')
 
+plt.figure()
+plt.plot(lms[:,0], lms[:,1], 'kx', label='landmarks')
+plt.plot(poses[:,0], poses[:,1], 'b', label='estimates')
+plt.legend()
+plt.show()
