5d99488d1c3ca1da6576bf45e848cbd9bee379df
Brendon Forsgren <bforsgren29@gmail.com>
2019-12-13 09:18:44 -0700

    Now plotting truth from an external file

--- python/plot_results.py
+++ python/plot_results.py
@@ -1,12 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from extractdata import *
 
 lms = np.loadtxt('/tmp/MHE_landmarks.txt')
 poses = np.loadtxt('/tmp/MHE_outputs.txt')
-print(lms)
 
 plt.figure()
 plt.plot(lms[:,1], lms[:,0], 'kx', label='landmarks')
-plt.plot(poses[:,0], poses[:,1], 'b', label='estimates')
+plt.plot(poses[:,1], poses[:,0], 'b', label='estimates')
+plt.plot(x_truth, y_truth, 'g', label='truth')
 plt.legend()
 plt.show()
