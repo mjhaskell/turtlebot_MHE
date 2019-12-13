c402894be6f66abf766a2327990c4c91346c5ef3
Brendon Forsgren <bforsgren29@gmail.com>
2019-12-13 08:55:45 -0700

    The x values for landmarks were in the second column and y values in the first

--- python/plot_results.py
+++ python/plot_results.py
@@ -3,9 +3,10 @@ import matplotlib.pyplot as plt
 
 lms = np.loadtxt('/tmp/MHE_landmarks.txt')
 poses = np.loadtxt('/tmp/MHE_outputs.txt')
+print(lms)
 
 plt.figure()
-plt.plot(lms[:,0], lms[:,1], 'kx', label='landmarks')
+plt.plot(lms[:,1], lms[:,0], 'kx', label='landmarks')
 plt.plot(poses[:,0], poses[:,1], 'b', label='estimates')
 plt.legend()
 plt.show()
